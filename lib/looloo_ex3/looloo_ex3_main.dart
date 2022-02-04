b6b474c840f3f92a162ed0b959a1b23cd7e424a5
worapong-it <74828411+worapong-it@users.noreply.github.com>
2022-02-04 23:11:25 +0700

    add partial layout of page3 and page4

--- lib/looloo_ex3/looloo_ex3_main.dart
+++ lib/looloo_ex3/looloo_ex3_main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 // import 'package:provider/provider.dart';
 import '../custom_drawer.dart';
+import 'dart:math';
 
 class MyThirdPage extends StatelessWidget {
   const MyThirdPage({Key? key}) : super(key: key);
@@ -12,7 +13,26 @@ class MyThirdPage extends StatelessWidget {
         title: Text('Looloo - Exercise3'),
       ),
       drawer: CustomDrawer(),
-      body: Text('This is body. xxx'),
+      body: Center(
+        child: Column(
+          children: [
+            Text('Network image here...'),
+            SizedBox(height: 20),
+            Text('This is body. xxx'),
+            SizedBox(height: 20),
+            CircularProgressIndicator(),
+            SizedBox(height: 20),
+            Text(getRandomNumber().toString()),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Future<int> getRandomNumber() {
+    return Future.delayed(
+      Duration(seconds: 2),
+      () => Random().nextInt(50000),
     );
   }
 }
--- lib/looloo_ex4/looloo_ex4_main.dart
+++ lib/looloo_ex4/looloo_ex4_main.dart
@@ -12,7 +12,124 @@ class MyForthPage extends StatelessWidget {
         title: Text('Looloo - Exercise4'),
       ),
       drawer: CustomDrawer(),
-      body: Text('This is body. yyy'),
+      body: Stack(
+        children: [
+          GreenBackground(),
+          Center(
+            child: Column(
+              children: [
+                SizedBox(height: 15),
+                Text(
+                  'ถอนเงิน',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 28,
+                  ),
+                ),
+                SizedBox(height: 15),
+                BalanceDisplay(),
+                SizedBox(height: 15),
+                AccountDisplay(),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+// --- AccountDisplay --- //
+class AccountDisplay extends StatelessWidget {
+  const AccountDisplay({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          'บัญชีรับโอน',
+          style: TextStyle(
+            fontWeight: FontWeight.bold,
+            fontSize: 18,
+          ),
+        ),
+        SizedBox(height: 10),
+        ClipRRect(
+          borderRadius: BorderRadius.circular(15),
+          child: Container(
+            height: 100,
+            width: 350,
+            decoration: BoxDecoration(color: Colors.white),
+            child: Padding(
+              padding: const EdgeInsets.all(20),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text('ธนาคารกสิกรไทย'),
+                  Text('ชื่อบัญชี: สมชาย ขยันดี'),
+                  Text('เลขที่บัญชี: 1234567890'),
+                ],
+              ),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+// --- BalanceDisplay --- //
+class BalanceDisplay extends StatelessWidget {
+  const BalanceDisplay({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(15),
+      child: Container(
+        height: 100,
+        width: 350,
+        decoration: BoxDecoration(color: Colors.white),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text('ยอดเงินที่ถอนได้'),
+            Text('1,470 บาท'),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+// --- GreenBackground --- //
+class GreenBackground extends StatelessWidget {
+  const GreenBackground({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Expanded(
+          flex: 1,
+          child: Container(
+            decoration: BoxDecoration(color: Colors.teal[400]),
+          ),
+        ),
+        Expanded(
+          flex: 3,
+          child: Container(
+            decoration: BoxDecoration(color: Colors.grey[300]),
+          ),
+        ),
+      ],
     );
   }
 }
