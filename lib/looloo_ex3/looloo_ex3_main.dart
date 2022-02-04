824df7a1c68eb6352e7c28ca61a3216545c62546
worapong-it <74828411+worapong-it@users.noreply.github.com>
2022-02-05 01:10:24 +0700

    mockup waiting (async, await)

--- lib/looloo_ex3/looloo_ex3_main.dart
+++ lib/looloo_ex3/looloo_ex3_main.dart
@@ -3,29 +3,59 @@ import 'package:flutter/material.dart';
 import '../custom_drawer.dart';
 import 'dart:math';
 
-class MyThirdPage extends StatelessWidget {
+class MyThirdPage extends StatefulWidget {
   const MyThirdPage({Key? key}) : super(key: key);
 
   @override
+  State<MyThirdPage> createState() => _MyThirdPageState();
+}
+
+class _MyThirdPageState extends State<MyThirdPage> {
+  int randomNumber = 0;
+  bool _isFinished = false;
+
+  Future func() async {
+    randomNumber = await getRandomNumber();
+    _isFinished = true;
+    setState(() {});
+  }
+
+  @override
   Widget build(BuildContext context) {
+    // final int randomNumber = await getRandomNumber();
+    // func();
+    Widget _body = Center(
+      child: Column(
+        children: [
+          Text('Network image here...'),
+          SizedBox(height: 20),
+          Text('This is body. xxx'),
+          SizedBox(height: 20),
+          // CircularProgressIndicator(),
+          SizedBox(height: 20),
+          Text(randomNumber.toString()),
+        ],
+      ),
+    );
+
+    /* call async function here */
+    if (!_isFinished) {
+      func();
+      _body = Center(
+        child: LimitedBox(
+          maxHeight: 50,
+          maxWidth: 50,
+          child: CircularProgressIndicator(),
+        ),
+      );
+    }
+
     return Scaffold(
       appBar: AppBar(
         title: Text('Looloo - Exercise3'),
       ),
       drawer: CustomDrawer(),
-      body: Center(
-        child: Column(
-          children: [
-            Text('Network image here...'),
-            SizedBox(height: 20),
-            Text('This is body. xxx'),
-            SizedBox(height: 20),
-            CircularProgressIndicator(),
-            SizedBox(height: 20),
-            Text(getRandomNumber().toString()),
-          ],
-        ),
-      ),
+      body: _body,
     );
   }
 
