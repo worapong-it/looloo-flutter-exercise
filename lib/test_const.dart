90150d7230e4517af324a9b28caf290dec92be09
worapong-it <74828411+worapong-it@users.noreply.github.com>
2022-02-04 15:22:46 +0700

    add test_const.dart file

---
+++ lib/test_const.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class LessWidget extends StatelessWidget {
+  const LessWidget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
+/*  */
+
+class FulWidget extends StatefulWidget {
+  const FulWidget({Key? key}) : super(key: key);
+
+  @override
+  _FulWidgetState createState() => _FulWidgetState();
+}
+
+class _FulWidgetState extends State<FulWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
